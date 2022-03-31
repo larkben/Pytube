eefe44a0358fe7cd9f9b5349e4ff61e7757f38ca
Benjamin Larkey <60051088+larkben@users.noreply.github.com>
2022-03-31 08:29:05 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,43 @@
+import pytube
+from pytube import YouTube
+import tkinter as tk
+from tkinter import *
+from tkinter import ttk
+
+# intitalize
+
+root = tk.Tk()
+
+url_entry = ttk.Entry(root)
+url_entry.pack()
+
+def DownloadMP4():
+    video_object = YouTube(str(url_entry.get()))
+    video_object.streams.get_highest_resolution().download()
+
+def DownloadMP3():
+    video_object = YouTube(str(url_entry.get()))
+    video_object.streams.get_audio_only().download()
+
+button_mp4 = ttk.Button(root, text="Download MP4", command=DownloadMP4)
+button_mp4.pack()
+
+button_mp3 = ttk.Button(root, text="Download MP3", command=DownloadMP3)
+button_mp3.pack()
+
+# video information
+'''
+print(video_object.title)
+print(f'{video_object.length / 60} minutes')
+print(video_object.views)
+print(video_object.author)
+print(video_object.description)
+'''
+
+# video streams
+
+#download
+
+#closing
+
+root.mainloop()
